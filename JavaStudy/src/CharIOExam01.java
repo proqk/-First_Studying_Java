a2589cf55332c1c3b4c5d2604de1b47d7603bd83
proqk <proqk2@gmail.com>
2017-12-13 17:01:18 +0900

    Revert "no message"
    This reverts commit 3fb7c67686ef841595077f329058d806aa2d0b93.

--- JavaStudy/src/CharIOExam01.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class CharIOExam01 {
-    public static void main(String[] args) throws IOException{
-        char a = (char)System.in.read();
-        System.out.println(a);
-    }
-}
